e5996e6517cb9f294964bae722a1803faff4fa7b
Gilles Dubois <gillesdubois@users.noreply.github.com>
2017-06-11 20:37:27 +0200

    Inital commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		GameObject player = new GameObject ();
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update after everyobject has been process in update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody playerRb;
+	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start (){
+		count = 0;
+		playerRb.GetComponent<Rigidbody>();
+		float speed;
+		this.setCountText ();
+		winText.text = "";
+	}
+
+	// Update before rendering every frame
+	void Update (){
+
+	}
+
+	// Physics calculation related update
+	void FixedUpdate (){
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		playerRb.AddForce (movement * speed);
+	}
+
+	// On colision detect
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			this.setCountText ();
+		}
+	}
+
+	void setCountText(){
+		countText.text = "Count " + count.ToString ();
+		if(count >= 13){
+			winText.text = "You win";
+		}
+	}
+}
